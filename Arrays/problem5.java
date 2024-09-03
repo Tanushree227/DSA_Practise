d4301bbada920c700663a9eed4eeb02bcd742cc4
Tanushree227 <tanushreedas813@gmail.com>
2024-09-03 20:49:20 +0530

    problem 5: Remove Duplicates from a Sorted Array

---
+++ Arrays/problem5.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class problem5 {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 3, 4, 4, 5};
+        int n = arr.length;
+
+        if (n == 0 || n == 1) {
+            System.out.println("Array with unique elements: " + Arrays.toString(arr));
+            return;
+        }
+
+        int[] temp = new int[n];
+        int j = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                temp[j++] = arr[i];
+            }
+        }
+
+        temp[j++] = arr[n - 1];
+
+        int[] result = Arrays.copyOf(temp, j);
+
+        System.out.println("Array with unique elements: " + Arrays.toString(result));
+    }
+}
