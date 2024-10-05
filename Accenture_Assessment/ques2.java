bc7adb82804dac62614624b1e070f58f7b9b9ed2
Tanushree227 <tanushreedas813@gmail.com>
2024-10-05 20:59:20 +0530

    ques 2

---
+++ Accenture_Assessment/ques2.java
@@ -0,0 +1,8 @@
+//Arun is a bus conductor. His ticket machine is printing numbers in reverse order due to a technical glitch. As a programmer on the bus, you are asked to help him by creating a program to display the numbers correctly.
+//Eg:
+//Input: 320
+//Output: Number in reversed order: 23
+
+public class ques2 {
+    
+}
