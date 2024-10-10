ae4e6ddaa68656d307b295b66456484f8267e224
Tanushree227 <tanushreedas813@gmail.com>
2024-10-10 11:43:51 +0530

    better approach

--- Arrays_DSA/sum_array.java
+++ Arrays_DSA/sum_array.java
@@ -35,7 +35,55 @@ public class sum_array {
         else
             System.out.println("false");
     }
+
+    // Function to perform binary search
+    static boolean binarySearch(int[] arr, int left,
+                                int right, int target){
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target)
+                return true;
+            if (arr[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+    }
+
+    //Better Approach
+    // Function to check whether any pair exists
+    // // whose sum is equal to the given target value
+    // static boolean twoSum(int[] arr, int target){
+    //     // Sort the array
+    //     Arrays.sort(arr);
+
+    //     // Iterate through each element in the array
+    //     for (int i = 0; i < arr.length; i++) {
+    //         int complement = target - arr[i];
+
+    //         // Use binary search to find the complement
+    //         if (binarySearch(arr, i + 1, arr.length - 1,
+    //                          complement))
+    //             return true;
+    //     }
+    //     // If no pair is found
+    //     return false;
+    // }
+
+    // public static void main(String[] args){
+    //     int[] arr = { 0, -1, 2, -3, 1 };
+    //     int target = -2;
+
+    //     // Call the twoSum function and print the result
+    //     if (twoSum(arr, target)) {
+    //         System.out.println("true");
+    //     }
+    //     else {
+    //         System.out.println("false");
+    //     }
+    // }
 }
- {
-    
-}
+
+
